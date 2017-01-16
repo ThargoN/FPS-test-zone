f2271ae8b2df7b8f3ba2f0fd7b930ca14e6dad36
ThargoN <thargonsg@gmail.com>
2017-01-17 01:05:45 +0300

    Experiments with GUI camera.

--- Assets/Standard Assets/Effects/ImageEffects/Scripts/ScreenOverlay.cs
+++ Assets/Standard Assets/Effects/ImageEffects/Scripts/ScreenOverlay.cs
@@ -3,52 +3,52 @@ using UnityEngine;
 
 namespace UnityStandardAssets.ImageEffects
 {
-    [ExecuteInEditMode]
-    [RequireComponent (typeof(Camera))]
-    [AddComponentMenu ("Image Effects/Other/Screen Overlay")]
-    public class ScreenOverlay : PostEffectsBase
+	[ExecuteInEditMode]
+	[RequireComponent (typeof(Camera))]
+	[AddComponentMenu ("Image Effects/Other/Screen Overlay")]
+	public class ScreenOverlay : PostEffectsBase
 	{
-	    public enum OverlayBlendMode
+		public enum OverlayBlendMode
 		{
-            Additive = 0,
-            ScreenBlend = 1,
-            Multiply = 2,
-            Overlay = 3,
-            AlphaBlend = 4,
-        }
+			Additive = 0,
+			ScreenBlend = 1,
+			Multiply = 2,
+			Overlay = 3,
+			AlphaBlend = 4,
+		}
 
-        public OverlayBlendMode blendMode = OverlayBlendMode.Overlay;
-        public float intensity = 1.0f;
-        public Texture2D texture = null;
+		public OverlayBlendMode blendMode = OverlayBlendMode.Overlay;
+		public float intensity = 1.0f;
+		public Texture2D texture = null;
 
-        public Shader overlayShader = null;
-        private Material overlayMaterial = null;
+		public Shader overlayShader = null;
+		private Material overlayMaterial = null;
 
 
-        public override bool CheckResources ()
+		public override bool CheckResources ()
 		{
-            CheckSupport (false);
+			CheckSupport (false);
 
-            overlayMaterial = CheckShaderAndCreateMaterial (overlayShader, overlayMaterial);
+			overlayMaterial = CheckShaderAndCreateMaterial (overlayShader, overlayMaterial);
 
-            if	(!isSupported)
-                ReportAutoDisable ();
-            return isSupported;
-        }
+			if	(!isSupported)
+				ReportAutoDisable ();
+			return isSupported;
+		}
 
-        void OnRenderImage (RenderTexture source, RenderTexture destination)
+		void OnRenderImage (RenderTexture source, RenderTexture destination)
 		{
-            if (CheckResources() == false)
+			if (CheckResources() == false)
 			{
-                Graphics.Blit (source, destination);
-                return;
-            }
+				Graphics.Blit (source, destination);
+				return;
+			}
 
-            Vector4 UV_Transform = new  Vector4(1, 0, 0, 1);
+			Vector4 UV_Transform = new  Vector4(1, 0, 0, 1);
 
 			#if UNITY_WP8
-	    	// WP8 has no OS support for rotating screen with device orientation,
-	    	// so we do those transformations ourselves.
+			// WP8 has no OS support for rotating screen with device orientation,
+			// so we do those transformations ourselves.
 			if (Screen.orientation == ScreenOrientation.LandscapeLeft) {
 				UV_Transform = new Vector4(0, -1, 1, 0);
 			}
@@ -60,10 +60,10 @@ namespace UnityStandardAssets.ImageEffects
 			}
 			#endif
 
-            overlayMaterial.SetVector("_UV_Transform", UV_Transform);
-            overlayMaterial.SetFloat ("_Intensity", intensity);
-            overlayMaterial.SetTexture ("_Overlay", texture);
-            Graphics.Blit (source, destination, overlayMaterial, (int) blendMode);
-        }
-    }
+			overlayMaterial.SetVector("_UV_Transform", UV_Transform);
+			overlayMaterial.SetFloat ("_Intensity", intensity);
+			overlayMaterial.SetTexture ("_Overlay", texture);
+			Graphics.Blit (source, destination, overlayMaterial, (int) blendMode);
+		}
+	}
 }
