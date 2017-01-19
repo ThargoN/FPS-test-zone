1fb8a931ca40e828a608e5b844c3ad17d3383faa
ThargoN <thargonsg@gmail.com>
2017-01-20 00:14:10 +0300

    Continuing experiments
    :one: Revert to Gamma color space - Linear works incorrect atm
    :two: Asset relocation
    :three: Upgrade to Unity 5.0.6 b4

---
+++ Assets/General/Utility/GameController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class GameController : Singleton<GameController> {
+
+	// Unity callbacks /////////////////////////////////////////////////////////////////////////////////////////
+	void Start () {
+	
+	}
+		
+	void Update() {
+		// Выход. Пока вот такой вот простой
+		if (Input.GetButtonUp( "Quit" )) {
+			QuitApplication();
+		}
+
+	}
+
+	void OnEnable() {
+		IsNoCursorMode = true;
+	}
+
+	void OnDisable() {
+		IsNoCursorMode = false;
+	}
+
+
+	// Properties //////////////////////////////////////////////////////////////////////////////////////////////
+	public bool IsCursorLocked {
+		set {
+			Cursor.lockState = value ? CursorLockMode.Locked : CursorLockMode.None;
+			Cursor.visible = !value;
+		}
+		get {
+			return !Cursor.visible;
+		}
+		
+	}
+	
+	/// <summary>
+	/// true если мы в режиме убранной мыши
+	/// </summary>
+	public bool IsNoCursorMode { get; set; }
+
+
+	// Public methods //////////////////////////////////////////////////////////////////////////////////////////
+	public void QuitApplication() {
+#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+#else
+		Application.Quit();
+#endif
+	}
+
+}
